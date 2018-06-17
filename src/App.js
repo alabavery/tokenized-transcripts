5b4d94302ce4bf203e806238eb7dea08853d231c
al.avery <al.avery.dev@gmail.com>
2018-06-16 20:49:06 -0500

    d

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import './App.css';
 import Dropzone from 'react-dropzone';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.dropzone = React.createRef();
+  }
+  
+  onUpload = files => {
+    console.log(files);
+    const audioiElement = document.createElement("AUDIO");
+  };
+  
   render() {
     return (
       <div className="App">
+        <button
+          type="primary"
+          onClick={() => {this.dropzone.current.open();}}>
+          Click
+        </button>
+
+        <Dropzone accept=".mp3,.wav" ref={this.dropzone} style={{ display: 'none' }} onDrop={this.onUpload} />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
